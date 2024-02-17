bd61746766623afb8fdeaf617de7b901627e7a0c
tuva9 <tuva9@fpt.edu.vn>
2024-02-17 21:52:43 +0700

    Delete product

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,21 @@
+// Code dong bo
 const table = document.querySelector("#table")
+// Code bat dong bo
 fetch("http://localhost:3000/products")
     .then(res => res.json())
     .then(data => render(data))
+    .then(() => {
+        // Code dong bo
+        const deleteBtns = document.querySelectorAll(".delete-btn")
+        for (let btn of deleteBtns) {
+            btn.addEventListener('click', function () {
+                const id = btn.dataset.id
+                fetch("http://localhost:3000/products/" + id, {
+                    method: "DELETE",
+                })
+            })
+        }
+    })
 
 function render(data) {
     let text = ""
@@ -14,7 +28,7 @@ function render(data) {
                 <td><img src=${product.image}/></td>
                 <td>${product.price}</td>
                 <td>
-                    <button style="background-color: red">Xoá</button>
+                    <button class="delete-btn" data-id=${product.id} style="background-color: red">Xoá</button>
                     <button style="background-color: blue">Cập nhật</button>
                 </td>
             </tr>
